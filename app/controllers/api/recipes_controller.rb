e32b763564c7f16006d7aa1dee79f80569301d3a
tkt <tktcorporation.go@gmail.com>
2019-01-01 10:48:22 +0900

    公開設定になっているレシピのみを返すように
    公開設定になっているレシピのみ返すように
    
    公開設定になっているレシピのみ返すように

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,10 +1,10 @@
 class Api::RecipesController < ApplicationController
 
   def index
-    @recipes = Recipe.all.order(boosted: :desc)
+    @recipes = Recipe.where(publish: true).order(boosted: :desc)
   end
 
   def show
-    @recipe = Recipe.includes(:recipe_ingredients, :instructions).find(params[:id])
+    @recipe = Recipe.includes(:recipe_ingredients, :instructions).find_by(id: params[:id], publish: true)
   end
 end
