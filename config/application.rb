9b2bf94b6b7426e1edf530d294856cbbea597f8e
tkt <tktcorporation.go@gmail.com>
2019-01-01 18:36:26 +0900

    ローカライズ

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Neptune
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
+
     config.generators do |g|
       g.helper false
       g.assets false
