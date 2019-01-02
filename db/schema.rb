44ed7e96fb939ab6562ff21cc640d61ee5ada84f
tkt <tktcorporation.go@gmail.com>
2019-01-02 18:16:15 +0900

    user認証用のdb構成を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181226174943) do
+ActiveRecord::Schema.define(version: 2018_12_26_174943) do
 
-  create_table "ingredients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "ingredients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.float "salt", limit: 24
-    t.float "calorie", limit: 24
-    t.float "density", limit: 24
+    t.float "salt"
+    t.float "calorie"
+    t.float "density"
     t.integer "g_per_unit"
     t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
   end
 
-  create_table "instructions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "instructions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "recipe_id", null: false
     t.integer "sort_order", null: false
     t.text "body", null: false
@@ -31,40 +31,49 @@ ActiveRecord::Schema.define(version: 20181226174943) do
     t.index ["recipe_id"], name: "index_instructions_on_recipe_id"
   end
 
-  create_table "recipe_ingredients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "recipe_ingredients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "recipe_id", null: false
     t.integer "sort_order", null: false
     t.string "name", null: false
     t.integer "ingredient_id"
     t.string "quantity_and_unit", null: false
-    t.float "density", limit: 24
-    t.float "calorie", limit: 24
+    t.float "density"
+    t.float "calorie"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "unit_id"
-    t.float "quantity", limit: 24
+    t.float "quantity"
     t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
-  create_table "recipes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.string "video_url", null: false
     t.string "thumbnail_url", null: false
     t.integer "servings", null: false
-    t.float "salt", limit: 24
-    t.float "calorie", limit: 24
-    t.text "introduction"
+    t.float "salt"
+    t.float "calorie"
+    t.text "introduction", null: false
     t.boolean "boosted", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "publish", default: false
+    t.boolean "publish", default: false, null: false
     t.index ["boosted"], name: "index_recipes_on_boosted"
   end
 
-  create_table "units", id: :integer, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name", limit: 100
+  create_table "units", id: :integer, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", limit: 100, default: "", null: false
     t.boolean "position", comment: "0が先頭数字、1が後ろに数字"
-    t.float "volume", limit: 24
+    t.float "volume"
+  end
+
+  create_table "users", id: :integer, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "password_digest", default: "", null: false, collation: "utf8_bin"
+    t.string "auth_token", collation: "utf8_bin"
+    t.datetime "expiration_date"
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }
+    t.datetime "updated_at"
   end
 
 end
