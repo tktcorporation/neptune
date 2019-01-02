986c310346d4afdbce049304d9312796a24a3d53
tkt <tktcorporation.go@gmail.com>
2019-01-02 18:16:58 +0900

    ユーザー認証、トークン認証の設定を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :recipes, only: [:index]
   namespace :api, format: 'json' do
+    resources :users, only: [:create] do
+      collection do
+        post :get_auth_token
+      end
+    end
     resources :recipes, only: [:index, :show]
   end
   namespace :admin do
